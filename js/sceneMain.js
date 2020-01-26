55417e06939d8b58ed0dfafadc6fd97122f232cb
Marcin <marcingorecki1981@gmail.com>
2020-01-26 19:58:19 +0100

    Collision sprite-tween

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -18,25 +18,25 @@ class SceneMain extends Phaser.Scene {
       frameWidth: 36,
       frameHeight: 56
     });
+
+    //load spider - for tween
+    this.load.spritesheet("spider", "images/spider.png", {
+      frameWidth: 64,
+      frameHeight: 64
+    });
   }
 
   create() {
     //display background and set its left top corner in the browser's corner
     this.add.image(0, 0, "background").setOrigin(0, 0);
 
+    //bottom floor
     const platforms = this.physics.add.staticGroup();
     for (let a=0; a<=1333; a+=200) {
       platforms.create(a+30, game.config.height-10, 'floor')
     }
-    // platforms.create(100, game.config.height-40, 'floor');
-    // platforms.create(50, 250, 'floor');
-    // platforms.create(750, 220, 'floor');
-    // platforms.create(300, game.config.height-40, 'floor');
-    // platforms.create(500, game.config.height-63, 'floor');
-    // platforms.create(700, game.config.height-86, 'floor');
-
-
-
+    //additional platforms
+    platforms.create(200, 560, 'floor');
 
     //play the sound
     this.music = this.sound.add('music');
@@ -46,20 +46,23 @@ class SceneMain extends Phaser.Scene {
     //Listen to keys
     window.cursors = this.input.keyboard.createCursorKeys();
 
-    //set main camera
-    const camera = this.cameras.main;
-    //set background color with camera
-    camera.setBackgroundColor("rgba(240, 125, 255, 1)");
-
     //add sprite and make it available for game physics engine
     this.dracula = this.physics.add.sprite(0, game.config.height - 80, "dracula");
-    this.dracula.setBounce(.1);
     //dracula won't come outside screen
     this.dracula.setCollideWorldBounds(true);
+
+    //spider is a tween but must be added to physics for collision detection
+    this.spider = this.physics.add.sprite(200, 600, "spider");
+    const spiderWalk = this.anims.generateFrameNumbers("spider");
+
+    //gravity has no effect on spider
+    this.spider.body.setGravityY(-game.config.physics.arcade.gravity.y);
     
-    
-    // this.dracula.body.setGravityY(1000);
     this.physics.add.collider(this.dracula, platforms);
+
+    //this detects when Dracula touches enemy (this.spider)
+    // this.physics.add.overlap(this.dracula, this.spider, enemyTouched, null, this);
+    this.physics.add.collider(this.dracula, this.spider, enemyTouched, null, this);
 
     const draculaWalk = this.anims.generateFrameNumbers("dracula"); //this generates frames from sprite sheet
 
@@ -67,18 +70,50 @@ class SceneMain extends Phaser.Scene {
     // console.log(this.add.displayList)
     // const thisXratio = window.innerWidth/1920;
     // const thisYratio = window.innerHeight/1080;
-    // this.add.displayList.list.forEach(el => {
-    //   el.scaleX = thisXratio;
-    //   el.scaleY = thisYratio;
-    //   // el.setDisplaySize(thisXratio, thisYratio);
-    // })
-
+    //this.add.displayList.list.forEach(el => {
+     // console.log(el)
+      // el.scaleX = thisXratio;
+      // el.scaleY = thisYratio;
+      // el.setDisplaySize(thisXratio, thisYratio);
+    //})
     this.anims.create({
       key: "walk",
       frames: draculaWalk,
       frameRate: 10, //speed
       repeat: -1 //infinite movement
     });
+
+    this.anims.create({
+      key: "spiderWalk",
+      frames: spiderWalk,
+      frameRate: 6, //speed
+      repeat: -1 //infinite movement
+    });
+    this.spider.play("spiderWalk");
+    this.tweens.add({
+      targets: this.spider,
+      duration: 800,
+      x: 200,
+      y: game.config.height-40,
+      yoyo: true,
+      repeat: -1
+    });
+
+    //console.log(this.tweens, this.add.displayList)
+
+    //method that runs when dracula touches enemies
+    function enemyTouched(player, enemy) {
+      //console.log(enemy)
+      this.physics.pause();
+      player.setTint(0xff0000);
+      setTimeout(() => {
+        player.clearTint()
+        player.x = 0;
+        player.y = game.config.height - 80;
+        this.physics.resume();
+      }, 1000)
+      
+    }
   }
     
 
@@ -108,7 +143,6 @@ class SceneMain extends Phaser.Scene {
       this.dracula.anims.stop("walk", true);
       this.dracula.setVelocityX(0);
     }
-    console.log()
     if (cursors.space.isDown && this.dracula.body.touching.down) {
       this.dracula.setVelocityY(-400);
     }
